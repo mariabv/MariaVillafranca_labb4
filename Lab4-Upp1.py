a049503099316c53e637cb4a2693d6b942449033
mariabv <43582671+mariabv@users.noreply.github.com>
2018-09-25 19:32:34 +0200

    Add files via upload

---
+++ Lab4-Upp1.py
@@ -0,0 +1,9 @@
+inpf  = open('ordlista.txt', 'r')
+record = inpf.readlines()
+for i in range(len(record)):
+	ord = record[i].split('\n')
+	record[i]=ord[0]
+print (record[5])
+print (len(record))
+print (len(record[5]))
+input('press enter')
