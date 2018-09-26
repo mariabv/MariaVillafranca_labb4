17bf42e898db065e4e05334f324c76a4585d1ec7
Maria Villafranca <mariabv@kth.se>
2018-09-26 18:44:44 +0200

    Lab4-Upp4_1.py

---
+++ Lab4-Upp4_1.py
@@ -0,0 +1,32 @@
+#Finns elementet x i den sorterade listan li ?
+def binsok(li, x):
+    lo = 0
+    hi = len(li)-1
+    while lo <= hi:
+        mid = (lo+hi)//2
+        print(li[mid])
+        if x < li[mid]:
+            hi = mid - 1
+        elif x > li[mid]:
+            lo = mid + 1
+        else:
+            return True
+    return False
+def linsok(lista,elem):
+	res = False
+	for ord in lista:
+		if ord == elem:
+			res = True; break
+	return res
+inpf  = open('ordlista.txt', 'r')
+record = inpf.readlines()
+for i in range(len(record)):
+	ord = record[i].split('\n')
+	record[i]=ord[0]
+while True:
+	test=input('Ditt ord: ')
+	if binsok(record,test):
+		print (test+' finns'); continue
+	else:
+		print (test+' finns inte'); continue
+input('press enter')
